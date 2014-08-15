26686dd89b98537c965877badc5b892775e7c224
CarlZhou <wuxi8black@gmail.com>
2014-08-15 19:32:39 -0400

    init proj

---
+++ PicQuiz/PicQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicQuiz
+//
+//  Created by carlzhou on 8/15/14.
+//  Copyright (c) 2014 CZLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PicQuiz/PicQuizTests/PicQuizTests.m
@@ -0,0 +1,39 @@
+//
+//  PicQuizTests.m
+//  PicQuizTests
+//
+//  Created by carlzhou on 8/15/14.
+//  Copyright (c) 2014 CZLabs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PicQuizTests : XCTestCase
+
+@end
+
+@implementation PicQuizTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
